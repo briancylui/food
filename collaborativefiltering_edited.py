443af07a360db1a12eade72e6074e89bab36d0f6
Joel Joseph Dominic <joeljosephdominic@DN0a224236.SUNet>
2017-12-04 21:07:38 -0800

    newest

--- collaborativefiltering_edited.py
+++ collaborativefiltering_edited.py
@@ -1,5 +1,6 @@
 import json
 import random
+import math
 
 #reviews = []
 mean_rating = 0
@@ -37,14 +38,16 @@ with open('review_training.json', 'r') as f:
             mean_rating += data['stars']
             if data['business_id'] in businessesToCustomers:
         		if data['user_id'] not in businessesToCustomers[data['business_id']]:
-        			businessesToCustomers[data['business_id']].append((data['user_id'], data['stars']))
+        			businessesToCustomers[data['business_id']][data['user_id']] = data['stars']
             else:
-                businessesToCustomers[data['business_id']] = [(data['user_id'], data['stars'])]
+				businessesToCustomers[data['business_id']] = {}
+				businessesToCustomers[data['business_id']][data['user_id']] = data['stars']
             if data['user_id'] in customersToBusinesses:
         		if data['business_id'] not in customersToBusinesses[data['user_id']]:
-        			customersToBusinesses[data['user_id']].append((data['business_id'], data['stars']))
+					customersToBusinesses[data['user_id']][data['business_id']] = data['stars']
             else:
-                customersToBusinesses[data['user_id']] = [(data['business_id'], data['stars'])]
+                customersToBusinesses[data['user_id']] = {}
+                customersToBusinesses[data['user_id']][data['business_id']] = data['stars']
 mean_rating = mean_rating / float(count)
 
 business_ratings = {}
@@ -55,9 +58,9 @@ customer_regularization_constant = 0
 for business_id in businessesToCustomers:
     customerBase = businessesToCustomers[business_id]
     business_rating = 0
-    for cr in customerBase:
-        user_id, rating = cr
-        business_rating += (rating - mean_rating) # changed from rating - mean_rating
+    for user_id in customerBase:
+        rating = customerBase[user_id]
+        business_rating += rating # changed from rating - mean_rating
     business_rating = business_rating/float(len(customerBase) + business_regularization_constant)
     business_ratings[business_id] = business_rating
 print(business_ratings)
@@ -65,28 +68,56 @@ print(business_ratings)
 for user_id in customersToBusinesses:
     businessesFrequented = customersToBusinesses[user_id]
     customer_rating = 0
-    for business in businessesFrequented:
-        business_id, rating = business
-        customer_rating += (rating - mean_rating - business_ratings[business_id]) #changed from rating - mean_rating - business_ratings[business_id]
+    for business_id in businessesFrequented:
+        rating = businessesFrequented[business_id]
+        customer_rating += rating #changed from rating - mean_rating - business_ratings[business_id]
     customer_rating = customer_rating/float(len(businessesFrequented) + customer_regularization_constant)
     customer_ratings[user_id] = customer_rating
 print(customer_ratings)
 
-"""
+
 similarityDict = {}
 
 #calculate Pearson correlation
 for business_id_A in businessesToCustomers:
     for business_id_B in businessesToCustomers:
         if business_id_A != business_id_B:
-            similarity = 0
-            customers_A = businessesToCustomers[business_id_A]
-            customers_B = businessesToCustomers[business_id_B]
-            for customer_A in customers_A:
-                user_id, rating = customer_A
+			numerator = 0
+			denominator = 0
+			customers_A = businessesToCustomers[business_id_A]
+			customers_B = businessesToCustomers[business_id_B]
+			for user_id in customers_A:
+				if user_id in customers_B:
+					#used the similarity measure from milestone - edit if that isn't accurate here
+					numerator += (customers_A[user_id] - business_ratings[business_id_A]) * (customers_B[user_id] - business_ratings[business_id_B])
+					denominator += ((customers_A[user_id] - business_ratings[business_id_A]) ** 2) * ((customers_B[user_id] - business_ratings[business_id_B]) ** 2)
+			similarityDict[business_id_A] = {}
+			similarityDict[business_id_B] = {}
+			if numerator == 0:
+				similarityDict[business_id_A][business_id_B] = 0
+				similarityDict[business_id_B][business_id_A] = 0
+			else:
+				similarityDict[business_id_A][business_id_B] = numerator/float(math.sqrt(denominator))
+				similarityDict[business_id_B][business_id_A] = numerator/float(math.sqrt(denominator))
+
+#similarly used prediction formula from milestone
+#assumes business_id is mentioned in reviews.json
+def prediction(business_id, user_id, similarityDict, businessesToCustomers, business_ratings):
+	relevantBusinesses = similarityDict[business_id]
+	numerator = 0
+	denominator = 0
+	for business_id_2 in relevantBusinesses:
+		customerBase = businessesToCustomers[business_id_2]
+		if user_id in customerBase:
+			numerator += (businessesToCustomers[business_id_2][user_id] - business_ratings[business_id_2]) * similarityDict[business_id][business_id_2]
+			denominator += similarityDict[business_id][business_id_2]
+	if denominator == 0:
+		prediction = business_ratings[business_id]
+	else:
+		prediction = business_ratings[business_id] + (numerator/float(denominator))
+	return prediction
 
 ## final testing below
-"""
 """
 #actual testing
 finalscores = [0, 0] #accurate, notaccurate
@@ -113,24 +144,23 @@ print(accuracy)
 finalscores = [0, 0] #accurate, notaccurate
 with open('review_test.json', 'r') as f:
     for line in f:
-        data = json.loads(line)
-        predicted_rating = mean_rating
-        business_id = data['business_id']
-        user_id = data['user_id']
-        if business_id in businessIDList_training:
-            if business_id in business_ratings:
-                predicted_rating += business_ratings[business_id]
-            if user_id in customer_ratings:
-                predicted_rating += customer_ratings[user_id]
-            predicted_rating = round(predicted_rating)
-            if predicted_rating > 5:
-                predicted_rating = 5
-            if predicted_rating < 1:
-                predicted_rating = 1
-            print predicted_rating, data['stars']
-            if predicted_rating == data['stars']:
-                finalscores[0] += 1
-            else:
-                finalscores[1] += 1
+		predicted_rating = 0
+		data = json.loads(line)
+		user_id = data['user_id']
+		business_id = data['business_id']
+		if business_id in businessIDList_training:
+			if business_id in business_ratings:
+				if user_id in customer_ratings:
+					predicted_rating = prediction(business_id, user_id, similarityDict, businessesToCustomers, business_ratings)
+					predicted_rating = int(round(predicted_rating))
+					if predicted_rating > 5:
+						predicted_rating = 5
+					if predicted_rating < 1:
+						predicted_rating = 1
+						print predicted_rating, data['stars']
+					if predicted_rating == data['stars']:
+						finalscores[0] += 1
+					else:
+						finalscores[1] += 1
 accuracy = finalscores[0]/float(sum(finalscores))
 print(accuracy)
